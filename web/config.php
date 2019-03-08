18898cd1a3052d9b748cac82a0222b2dc1f28d8f
VPN-V2Ray <46552880+VPN-V2Ray@users.noreply.github.com>
2019-03-08 17:43:51 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
+define('APIKEY', getenv("AIzaSyBPLf8NYoQ8gCbht2_vM_DoBOl0pL4_3EE"));
 define('GJ_CODE', getenv("GJ_CODE"));
 define('SITE_NAME',getenv("SITE_NAME"));
 define('TITLENAME', getenv("TITLENAME"));
